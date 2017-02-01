67fe6892814f50d3e04ef4ef4f63713b47c27977
ntorin <xtoribear@gmail.com>
2017-02-01 11:12:19 -0800

    closes #1
    Signed-off-by: ntorin <xtoribear@gmail.com>

---
+++ application/controllers/first/Welcome.php
@@ -0,0 +1,26 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends Application
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Homepage for our app
+	 */
+	public function index()
+	{
+		// this is the view we want shown
+
+		// build the list of authors, to pass on to our view
+        $record = $this->quotes->get('1');
+        echo $record['what'];
+
+	}
+
+}
