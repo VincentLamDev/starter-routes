cfc7258940764925b39c2bb2a27e0566ee2946a1
Joshua Chan <jchan91913@gmail.com>
2017-02-01 11:21:32 -0800

    finished issue #4 lockemup

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,3 +53,4 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['last'] = '/last/Welcome/index';
+$route['lock/(:any)/(:any)'] = "welcome/shucks";
